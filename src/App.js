0097ff90ccadee03fdaeb0df578686826e71888d
Pushpraj Singh Jadoun <pushprajsinghjadoun@outlook.com>
2022-12-13 15:00:12 +0530

    Navbar added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,32 @@ import './App.css';
 import Registration from './components/Registration'
 import Loginbox from './loginc/Loginbox.js';
 import Logout from './logout/Logout'
-import Navbar from './navbar/Navbar'
+import Navs from './navbar/Navs'
+import React from 'react'
+import {
+      BrowserRouter as Router,
+      Routes,
+      Route,
+      Link
+  } from 'react-router-dom';
+import ReactDOM from "react-dom/client";
+
 
 
 function App() {
   return (
-    <Navbar/>
+    <React.Fragment>
+    <Router>
+           <div className="App">
+            <Navs/>
+           <Routes>
+                 <Route exact path='/' element={< Registration />}></Route>
+                 <Route exact path='/login' element={< Loginbox />}></Route>
+                 <Route exact path='/register' element={< Registration />}></Route>
+          </Routes>
+          </div>
+       </Router>
+    </React.Fragment>
   );
 }
 
---
+++ src/navbar/Navs.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import react from 'react'
+import {Outlet, Link} from 'react-router-dom'
+import styles from './Navbar.module.css'
+
+function Navs()
+{
+    return (
+        <React.Fragment>
+        <ul className={styles.ul}>
+        <li className={styles.li}>
+        <Link className={styles.links} to="/">Home</Link>
+        </li>
+        <li className={styles.li}>
+        <Link className={styles.links} to="/login">About Us</Link>
+        </li>
+        <li className={styles.li}>
+        <Link className={styles.links} to="/register">Logout</Link>
+        </li>
+        </ul>
+        </React.Fragment>
+    );
+}
+export default Navs;
